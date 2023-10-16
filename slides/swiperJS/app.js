3c1c06c08ab7fe877668b4b572d29b0bd969f3b6
GabrielMoraes <gugapoeta@gmail.com>
2023-10-16 16:27:39 -0400

    Set of slides for a client

--- slides/swiperJS/app.js
+++ slides/swiperJS/app.js
@@ -1,18 +1,24 @@
 const swiper = new Swiper('.container', {
+  
   effect: "coverflow",
   grabCursos: true,
   centeredSlides: true,
-  loop: true,
+  slidesPerView: 2,
+  initialSlide:1,
+  // loop: true,
   autoplay: true,
-  slidesPerView: 'auto',
+  keyboard: {
+    enabled: true,
+  },
   coverflowEffect: {
     rotate: 0,
     stretch: 1,
-    depth: 100,
-    modifier: 7
+    depth:100,
+    modifier: 7,
+    clickable: true,
+    
   },
   pagination: {
     el: '.swiper-pagination',
-    clickable: true
-  }
-      });
+    clickable: true,
+  }});
