43c17ab739c6eac067a0b4d2460ae3a627c369cf
Gabriel Poeta <gugapoeta@gmail.com>
2023-11-12 08:37:32 -0500

    Add intersectionObserver files

---
+++ intersectionObserver/index.js
@@ -0,0 +1,36 @@
+const cards = document.querySelectorAll(".card");
+
+
+const names = ['Gabriel', 'Aline', 'Cliff', 'Joey']
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("show", entry.isIntersecting)
+    // if(entry.isIntersecting) observer.unobserve(entry.target)
+  });
+},{
+  threshold: 1,
+  // rootMargin: '100px'
+  // root: ''
+}
+);
+
+
+cards.forEach(card =>{
+  observer.observe(card)
+
+})
+
+
+
+
+// let cardsKey = Object.entries(cards)
+
+// console.log(cards)
+
+
+// let newCards = cardsKey.map((card) => {
+//   return card
+// })
+
+// console.log(newCards)
